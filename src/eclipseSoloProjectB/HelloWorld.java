19a3082fb0b4bd59566ddee8f39e94eb5d51ec73
amack18-glitch <amack18@emich.edu>
2026-02-16 20:32:29 -0500

    added a message to programmer 2's print line statement

--- src/eclipseSoloProjectB/HelloWorld.java
+++ src/eclipseSoloProjectB/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String []args) {
 		System.out.println("Hello gitlab");
 		//comment
-		System.out.println("Programmer 2 did this");
+		System.out.println("Programmer 2 did this Programmer 1 did this ");
 	}
 
 }
